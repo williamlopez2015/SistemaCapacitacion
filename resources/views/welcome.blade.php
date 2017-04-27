f4503c36f82424264a2d46bd1df45d556093175c
Carlos Martínez <odioelcapital@hotmail.com>
2017-04-26 19:06:36 -0600

    Estructura básica de laravel y dompdf

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+ 
+<html lang="es">
+ 
+<head>
+<title>Sistema Gerencial de capacitación</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="estilos.css" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="alternate" title="Pozolería RSS" type="application/rss+xml" href="/feed.rss" />
+</head>
+ 
+<body>
+    <header>
+    <h1>Sistema Gerencial de Recursos Humanos de Recursos Humanos del Ministerio de Trabajo y Previsión Social</h1>
+       
+    </header>
+        <h2>Listado de Reportes Tácticos:</h2>
+       <ul>
+        <li><a href="pdf">Reporte de listado de temas por componente</a></li>
+        <li><a href="">Reporte del listado de empleados clasificados por departamento</a></li>
+        <li><a href="">Reporte de temas y componentes</a></li>
+        <li></li>
+        <li></li>
+        <li></li>
+       </ul>
+       <h2>Listado de Reportes Estratégicos:</h2>
+          <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+       </ul>
+  
+</body>
+</html>
