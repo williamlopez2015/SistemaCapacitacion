4df6b43e32f11e1a08b15656374cc92850a9103c
kratos <bautista.insu@gmail.com>
2017-04-27 22:28:41 -0600

    agregada la plantilla de trabajo

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Ministerio de Trabajo y Previsión Social</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<link href="{{asset('plantilla/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('plantilla/css/bootstrap-responsive.min.css')}}" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
+        rel="stylesheet">
+<link href="{{asset('plantilla/css/font-awesome.css')}}" rel="stylesheet">
+<link href="{{asset('plantilla/css/style.css')}}" rel="stylesheet">
+<link href="{{asset('plantilla/css/pages/dashboard.css')}}" rel="stylesheet">
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
+                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.html">Ministerio de Trabajo y Prevision Social </a>
+      <div class="nav-collapse">
+        <ul class="nav pull-right">
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-cog"></i> Cuenta <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Configuraciones</a></li>
+              <li><a href="javascript:;">Ayuda</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
+                            class="icon-user"></i> Usuario <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="javascript:;">Profile</a></li>
+              <li><a href="javascript:;">Salir</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!--/.nav-collapse --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /navbar-inner --> 
+</div>
+<!-- /navbar -->
+<div class="subnavbar">
+  <div class="subnavbar-inner">
+    <div class="container">
+      <ul class="mainnav">
+        <li class="active"><a href="index.html"><i class="icon-dashboard"></i><span>Inicio</span> </a> </li>
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-list-alt"></i><span>Estrategico</span> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Reporte 1</a></li>
+            <li><a href="#">Reporte 2</a></li>
+            <li><a href="#">Reporte 2</a></li>
+            <li><a href="#">Reporte 4</a></li>
+            <li><a href="#">Reporte 5</a></li>
+            <li><a href="#">Reporte 6</a></li>
+          </ul>
+        </li>
+        <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-list-alt"></i><span>Tactico</span> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Reporte 1</a></li>
+            <li><a href="#">Reporte 2</a></li>
+            <li><a href="#">Reporte 2</a></li>
+            <li><a href="#">Reporte 4</a></li>
+            <li><a href="#">Reporte 5</a></li>
+            <li><a href="#">Reporte 6</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /subnavbar-inner --> 
+</div>
+<!-- /subnavbar -->
+<div class="main">
+  <div class="main-inner">
+    <div class="container">
+       @yield('contenido')
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /main-inner --> 
+</div>
+<!-- /main -->
+<div class="extra">
+  <div class="extra-inner">
+    <div class="container">
+      <div class="row">
+                    <div class="span3">
+                        <h4>
+                            Something Legal</h4>
+                        <ul>
+                            <li><a href="javascript:;">Read License</a></li>
+                            <li><a href="javascript:;">Terms of Use</a></li>
+                            <li><a href="javascript:;">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <!-- /span3 -->
+                </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /extra-inner --> 
+</div>
+<!-- /extra -->
+<div class="footer">
+  <div class="footer-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span12"> &copy; 2017 <a href="#">Universidad de El Salvador</a>. </div>
+        <!-- /span12 --> 
+      </div>
+      <!-- /row --> 
+    </div>
+    <!-- /container --> 
+  </div>
+  <!-- /footer-inner --> 
+</div>
+<!-- /footer --> 
+<!-- Le javascript
+================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+@stack('scripts')
+<script src="{{asset('plantilla/js/jquery-1.7.2.min.js')}}"></script> 
+<script src="{{asset('plantilla/js/excanvas.min.js')}}"></script> 
+<script src="{{asset('plantilla/js/chart.min.js')}}" type="text/javascript"></script> 
+<script src="{{asset('plantilla/js/bootstrap.js')}}"></script>
+<script language="javascript" type="text/javascript" src="{{asset('plantilla/js/full-calendar/fullcalendar.min.js')}}"></script>
+ 
+<script src="{{asset('plantilla/js/base.js')}}"></script> 
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,79 @@
-<!DOCTYPE html>
- 
-<html lang="es">
- 
-<head>
-<title>Sistema Gerencial de capacitación</title>
-<meta charset="utf-8" />
-<link rel="stylesheet" href="estilos.css" />
-<link rel="shortcut icon" href="/favicon.ico" />
-<link rel="alternate" title="Pozolería RSS" type="application/rss+xml" href="/feed.rss" />
-</head>
- 
-<body>
-    <header>
-    <h1>Sistema Gerencial de Recursos Humanos de Recursos Humanos del Ministerio de Trabajo y Previsión Social</h1>
-       
-    </header>
-        <h2>Listado de Reportes Tácticos:</h2>
-       <ul>
-        <li><a href="pdf/1">Reporte de listado de temas por componente</a></li>
-        <li><a href="pdf/2">Reporte del listado de empleados clasificados por departamento</a></li>
-        <li><a href="pdf/3">Reporte de temas y componentes</a></li>
-        <li></li>
-        <li></li>
-        <li></li>
-       </ul>
-       <h2>Listado de Reportes Estratégicos:</h2>
-          <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-       </ul>
-  
-</body>
-</html>
+
+@extends('layouts.admin')
+@section('contenido')
+                 <!--Contenido -->
+                <div class="row">
+                  <div class="span12">
+            
+                    <div id="target-1" class="widget">
+                
+                    <div class="widget-content">
+                  
+                      <h1>¡Bienvenido!</h1>
+                    
+                      <p>Este es sistema informático Gerencial.</p> 
+                  
+                    </div> <!-- /widget-content -->
+                
+                    </div> <!-- /widget -->
+            
+                  </div> <!-- /span12 -->
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                  <div class="span6">
+                    <div class="widget widget-nopad">
+                      <div class="widget-header"> <i class="icon-list-alt"></i>
+                        <h3> Reportes</h3>
+                      </div>
+                      <!-- /widget-header -->
+                      <div class="widget-content">
+                        <div class="widget big-stats-container">
+                          <div class="widget-content">
+                            <h6 class="bigstats">Reportes Sencillos y facil de genera <a href="http://www.egrappler.com" target="_blank">EGrappler.com</a>.  These are some dummy lines to fill the area.</h6>
+                            <div id="big_stats" class="cf">
+                              <div class="stat"> <i class="icon-anchor"></i> <span class="value">851</span> </div>
+                              <!-- .stat -->
+                              
+                              <div class="stat"> <i class="icon-thumbs-up-alt"></i> <span class="value">423</span> </div>
+                              <!-- .stat -->
+                              
+                              <div class="stat"> <i class="icon-twitter-sign"></i> <span class="value">922</span> </div>
+                              <!-- .stat -->
+                              
+                              <div class="stat"> <i class="icon-bullhorn"></i> <span class="value">25%</span> </div>
+                              <!-- .stat --> 
+                            </div>
+                          </div>
+                          <!-- /widget-content --> 
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <!-- /widget -->
+                  </div>
+                  <!-- /span6 -->
+                  <div class="span6">
+                    <div class="widget">
+                      <div class="widget-header"> <i class="icon-bookmark"></i>
+                        <h3>Important Shortcuts</h3>
+                      </div>
+                      <!-- /widget-header -->
+                      <div class="widget-content">
+                        <div class="shortcuts"> <a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-list-alt"></i><span
+                                                  class="shortcut-label">Apps</span> </a><a href="javascript:;" class="shortcut"><i
+                                                      class="shortcut-icon icon-bookmark"></i><span class="shortcut-label">Bookmarks</span> </a><a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-signal"></i> <span class="shortcut-label">Reports</span> </a><a href="javascript:;" class="shortcut"> <i class="shortcut-icon icon-comment"></i><span class="shortcut-label">Comments</span> </a><a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-user"></i><span
+                                                          class="shortcut-label">Users</span> </a><a href="javascript:;" class="shortcut"><i
+                                                              class="shortcut-icon icon-file"></i><span class="shortcut-label">Notes</span> </a><a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-picture"></i> <span class="shortcut-label">Photos</span> </a><a href="javascript:;" class="shortcut"> <i class="shortcut-icon icon-tag"></i><span class="shortcut-label">Tags</span> </a> </div>
+                        <!-- /shortcuts --> 
+                      </div>
+                      <!-- /widget-content --> 
+                    </div>
+                    <!-- /widget -->
+                  
+                  </div>
+                  <!-- /span6 --> 
+              </div>
+              <!-- /row --> 
+    </div>
+@endsection
